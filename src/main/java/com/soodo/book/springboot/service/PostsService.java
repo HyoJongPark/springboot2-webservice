2c6d8cf6e2c1db51faff8e8c8d54e3132380af11
rio <75190035+HyoJongPark@users.noreply.github.com>
2022-02-03 20:30:31 +0900

    CHAPTER 04. 머스테치를 사용한 화면 구성 실습코드 추가

--- src/main/java/com/soodo/book/springboot/service/PostsService.java
+++ src/main/java/com/soodo/book/springboot/service/PostsService.java
@@ -2,12 +2,17 @@ package com.soodo.book.springboot.service;
 
 import com.soodo.book.springboot.domain.posts.Posts;
 import com.soodo.book.springboot.domain.posts.PostsRepository;
+import com.soodo.book.springboot.web.dto.PostsListResponseDto;
 import com.soodo.book.springboot.web.dto.PostsResponseDto;
 import com.soodo.book.springboot.web.dto.PostsSaveRequestDto;
 import com.soodo.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,10 +33,25 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(()
                 -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
