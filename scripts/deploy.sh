add1645823c865a6e688e0ed4ce8818afd1f8c3f
rio <75190035+HyoJongPark@users.noreply.github.com>
2022-02-17 11:33:22 +0900

    자동 배포 코드 수정
    deploy.sh 오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,7 +25,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 echo "> JAR NAME: $JAR_NAME"
 
 echo "> JAR_NAME 에 실행권한 추가"
-chomd +x $JAR_NAME
+chmod +x $JAR_NAME
 
 nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
